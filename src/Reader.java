da1ba7807a0507f4acd53a76d6229330c0dedf33
Lane <iamaperson620@gmail.com>
2021-02-19 15:53:57 -0600

    added delimiters on newlines

--- src/Reader.java
+++ src/Reader.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Reader {
@@ -12,13 +13,13 @@ public class Reader {
     public ProcessQueue getData() throws FileNotFoundException {
         ProcessQueue processQueue = new ProcessQueue();
         Scanner s = new Scanner(new File(this.fileName));
-        s.useDelimiter(",");
+        s.useDelimiter(",|\\r\\n|\\n|\\r");
 
         while(s.hasNext()) {
-            int at = Integer.parseInt(s.next());
+            int at = Integer.parseInt(s.next().trim());
             String pid = s.next().trim();
-            int st = Integer.parseInt(s.next());
-            int prty = Integer.parseInt(s.next());
+            int st = Integer.parseInt(s.next().trim());
+            int prty = Integer.parseInt(s.next().trim());
 
             Process p = new Process(at, pid, st, prty);
             processQueue.pushProcess(p);
