ac4c97c08184bae2274b4a50a36744898ae0371c
Lane <iamaperson620@gmail.com>
2021-02-19 15:54:04 -0600

    this works now

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         GUI gui = new GUI();
+        ProcessQueue pq;
 
         System.out.println("Spawned GUI.");
-        //return;
+        System.out.println("Input filename for processes:");
+
+        try {
+            pq = new Reader(new Scanner(System.in).next()).getData();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to open file. Terminating program...");
+            return;
+        }
+
+        while (pq.hasProcesses()) {
+            Process p = pq.popProcess();
+            System.out.println(p.getProcessID());
+        }
     }
 }
