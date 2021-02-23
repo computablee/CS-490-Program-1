d926e8f1523577c75070a85e55b4dfc5f4696cf5
harris-timothy <harris.tim608@gmail.com>
2021-02-23 01:25:47 -0600

    Added primitive GUI functionality.

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
+import java.awt.Color;
 
 public class GUI {
     private JFrame frame;
+    private JPanel panel;
+    private JButton startButton;
+    private JButton pauseButton;
+    private JLabel statusLabel;
 
     public GUI() {
         javax.swing.SwingUtilities.invokeLater(this::spawnGUI);
@@ -10,9 +15,36 @@ public class GUI {
     private void spawnGUI() {
         this.frame = new JFrame("CS 490 Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //comment
-        //comment number two
-        // i am way cooler than tim
+        this.panel = new JPanel();
+        panel.setLayout(null);
+
+        this.startButton = new JButton("Start System");
+        startButton.setBounds(30, 5, 125, 35);
+
+
+        this.pauseButton = new JButton("Pause System");
+        pauseButton.setBounds(175, 5, 125, 35);
+
+        this.statusLabel = new JLabel();
+        statusLabel.setText("System is Paused");
+        statusLabel.setBounds(320, 5, 125, 35);
+
+        startButton.addActionListener(e -> {
+            statusLabel.setText("System is Running");
+            // add more functionality here once i know wtf is going on
+        });
+
+        pauseButton.addActionListener(e -> {
+            statusLabel.setText("System is Paused");
+            // add more functionality here once i know wtf is going on
+        });
+
+        panel.setBorder(BorderFactory.createLineBorder(Color.black));
+        panel.add(startButton);
+        panel.add(pauseButton);
+        panel.add(statusLabel);
+        frame.setContentPane(panel);
+        frame.getContentPane().setBackground(Color.lightGray);
         frame.pack();
         frame.setSize(500, 500);
         frame.setVisible(true);
