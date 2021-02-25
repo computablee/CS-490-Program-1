70b4e7cf17883d51378233bf81f0ab80e9a083e9
harris-timothy <harris.tim608@gmail.com>
2021-02-24 18:18:07 -0600

    Implemented file selection from GUI

--- src/GUI.java
+++ src/GUI.java
@@ -5,7 +5,11 @@ import Backend.CPU;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GUI {
     private JFrame frame;
@@ -18,23 +22,25 @@ public class GUI {
     private JLabel timeUnitLabel;
     private JLabel unitLabel;
     private JLabel enterPrompt;
+    private JLabel filePrompt;
     private JTextField timeUnit;
+    private JTextField fileLocation;
     private JTextArea processDetails;
     private JTextArea systemStats;
     private JTable waitingProcessQueue;
     private JScrollPane processScrollPane;
     private Processor processor;
-    private ProcessQueue processQueue;
     private String[] tableColumnNames = {"Process Name", "Service Time"};
     private int unit = 100;
-
+    private ProcessQueue pq;
 
     public GUI() {
         javax.swing.SwingUtilities.invokeLater(this::spawnGUI);
-        this.processor = null;
+
     }
 
     private void spawnGUI() {
+
         this.frame = new JFrame("CS 490 Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -42,59 +48,77 @@ public class GUI {
         panel.setLayout(null);
 
         this.startButton = new JButton("Start System");
-        startButton.setBounds(30, 5, 125, 35);
-
+        startButton.setBounds(30, 80, 125, 35);
 
         this.pauseButton = new JButton("Pause System");
-        pauseButton.setBounds(175, 5, 125, 35);
+        pauseButton.setBounds(175, 80, 125, 35);
 
         this.statusLabel = new JLabel();
-        statusLabel.setText("System is Paused");
-        statusLabel.setBounds(320, 5, 125, 35);
+        statusLabel.setText("System is Idle");
+        statusLabel.setBounds(320, 80, 125, 35);
 
         this.queueLabel = new JLabel();
         queueLabel.setText("Waiting Process Queue");
-        queueLabel.setBounds(50, 75, 150, 35);
+        queueLabel.setBounds(50, 125, 150, 35);
 
         this.timeUnitLabel = new JLabel();
         timeUnitLabel.setText("1 time unit =");
-        timeUnitLabel.setBounds(265, 75, 125, 35);
+        timeUnitLabel.setBounds(265, 125, 125, 35);
 
         this.processDetails = new JTextArea();
-        processDetails.setBounds(240, 123, 200, 75);
+        processDetails.setBounds(240, 173, 200, 75);
         processDetails.setBackground(Color.yellow);
         processDetails.setBorder(BorderFactory.createLineBorder(Color.orange));
         processDetails.setEditable(false);
 
         this.processDetails = new JTextArea();
-        processDetails.setBounds(240, 123, 200, 75);
+        processDetails.setBounds(240, 173, 200, 75);
         processDetails.setBackground(Color.yellow);
         processDetails.setBorder(BorderFactory.createLineBorder(Color.orange));
         processDetails.setEditable(false);
-
-        // Don't know what this needs to display
-        // processDetails.append to add things here
-
 
         this.systemStats = new JTextArea();
-        systemStats.setBounds(20, 225, 420, 150);
+        systemStats.setBounds(20, 275, 420, 150);
         systemStats.setBorder(BorderFactory.createLineBorder(Color.black));
         systemStats.setEditable(false);
         // Don't know what this needs to display
         // systemStats.appends to add things here
 
+        this.filePrompt = new JLabel();
+        filePrompt.setText("Input the File Location of the processes: ");
+        filePrompt.setBounds(30, 5, 250, 35);
+
+        this.fileLocation = new JTextField();
+        fileLocation.setBounds(30, 33, 270, 25);
+
+        this.fileButton = new JButton("OK");
+        fileButton.setBounds(305, 33, 55, 25);
+
+        fileButton.addActionListener(e -> {
+            try {
+                //this is really dumb code that gets a console input from user for a file, then extracts the data from that file into a ProcessQueue object
+                pq = new Reader(new Scanner(fileLocation.getText()).next()).getData();
+                System.out.println(pq);
+            } catch (FileNotFoundException ex) {
+                //print the exception and retire the application
+                System.out.println("Unable to open file. Terminating program...");
+                System.exit(0);
+                return; //code will not compile without this redundant return
+            }
+        });
+
         this.unitLabel = new JLabel();
         unitLabel.setText("ms");
-        unitLabel.setBounds(395, 75, 125, 35);
+        unitLabel.setBounds(395, 125, 125, 35);
 
         this.enterPrompt = new JLabel();
         enterPrompt.setFont(new Font("", Font.PLAIN, 10));
         enterPrompt.setText("(Press Enter to Set New Unit)");
-        enterPrompt.setBounds(340, 97, 150, 35);
+        enterPrompt.setBounds(340, 145, 150, 35);
 
         this.timeUnit = new JTextField(1);
         timeUnit.setText("100");
-        timeUnit.setBounds(340, 75, 50, 35);
+        timeUnit.setBounds(340, 130, 50, 25);
         timeUnit.addActionListener(e -> {
             // Make this change the millisecsPerTime (Processor.java) parameter somehow
             // This action listener will trigger when the user hits Enter, btw
@@ -103,12 +127,10 @@ public class GUI {
 
         });
 
-
-
         Timer t = new Timer(1, e -> {
-            if(processQueue != null) {
-                ArrayList<Process> pq = processQueue.getQueue();
-                Process[] processQueueArr = pq.toArray(new Process[pq.size()]);
+            if(pq != null) {
+                ArrayList<Process> procq = pq.getQueue();
+                Process[] processQueueArr = procq.toArray(new Process[procq.size()]);
                 String[][] processQueue2dArr = new String[processQueueArr.length][2];
                 for(int i = 0; i < processQueueArr.length; i++) {
                     processQueue2dArr[i][0] = processQueueArr[i].getProcessID();
@@ -116,7 +138,7 @@ public class GUI {
                 }
                 this.waitingProcessQueue = new JTable(processQueue2dArr, tableColumnNames);
                 this.processScrollPane = new JScrollPane(waitingProcessQueue);
-                processScrollPane.setBounds(20, 110, 200, 100);
+                processScrollPane.setBounds(20, 160, 200, 100);
                 panel.add(processScrollPane);
 
             }
@@ -131,26 +153,22 @@ public class GUI {
                     processDetails.setText(" CPU0 \n Exec: Idle\n Time Remaining = " + processor.timeRemaining(0));
                 } else {
                     processDetails.setText(" CPU0 \n Exec: Idle\n Time Remaining = n/a");
+                    statusLabel.setText("System is Idle");
                 }
             }
         });
         j.start();
 
-
         startButton.addActionListener(e -> {
-            if (processor != null && !processor.isRunning()) {
-                processor.startProcessor();
-                statusLabel.setText("System is Running");
-
-
-            }
-            else if (processor != null) {
+            if (processor != null) {
                 processor.unpauseSystem();
-                statusLabel.setText("System is Running");
             }
             else {
-                statusLabel.setText("ERR: No input file");
+                processor = new Processor(1, unit, pq);
+                processor.startProcessor();
+
             }
+            statusLabel.setText("System is Running");
 
         });
 
@@ -162,9 +180,14 @@ public class GUI {
 
         });
 
+
+
         panel.setBorder(BorderFactory.createLineBorder(Color.black));
         panel.add(startButton);
         panel.add(pauseButton);
+        panel.add(filePrompt);
+        panel.add(fileLocation);
+        panel.add(fileButton);
         panel.add(statusLabel);
         panel.add(queueLabel);
         panel.add(timeUnitLabel);
@@ -180,13 +203,13 @@ public class GUI {
         frame.setVisible(true);
     }
 
-    public void setProcessor(Processor processor) {
+    /*public void setProcessor(Processor processor) {
         this.processor = processor;
     }
 
     public void setProcessQueue(ProcessQueue processQueue) {
         this.processQueue = processQueue;
-    }
+    }*/
 
     public int getTimeUnit() {
         return this.unit;
--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,9 @@ public class Main {
     public static void main(String[] args) {
         //spawn GUI
         GUI gui = new GUI();
-        //create a process queue
-        ProcessQueue pq;
 
         //output some info to user
         System.out.println("Spawned GUI.");
-        System.out.println("Input filename for processes:");
 
-        //calling Reader::getData can throw a FileNotFoundException, so we have to handle that
-        try {
-            //this is really dumb code that gets a console input from user for a file, then extracts the data from that file into a ProcessQueue object
-            pq = new Reader(new Scanner(System.in).next()).getData();
-        } catch (FileNotFoundException e) {
-            //print the exception and retire the application
-            System.out.println("Unable to open file. Terminating program...");
-            System.exit(0);
-            return; //code will not compile without this redundant return
-        }
-
-        //Create the processor with 1 CPU, 100 milliseconds per unit of time, and with all CPUs sharing the same ProcessQueue, pq
-        Processor processor = new Processor(1, gui.getTimeUnit(), pq);
-
-        gui.setProcessor(processor);
-        gui.setProcessQueue(pq);
     }
 }
