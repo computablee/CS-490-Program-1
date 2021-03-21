53246f52f754ad5632b82735777fb623a828603d
Lane <iamaperson620@gmail.com>
2021-03-20 20:18:33 -0500

    added effectively a struct for process statistics

--- src/Backend/ProcessStatistics.java
+++ src/Backend/ProcessStatistics.java
@@ -1,2 +1,55 @@
-package Backend;public class ProcessStatistics {
+package Backend;
+
+public class ProcessStatistics {
+    private Process process;
+    private int arrivalTime;
+    private int serviceTime;
+    private Integer finishTime;
+    private Integer tat;
+    private Integer ntat;
+
+    public ProcessStatistics(Process process) {
+        this.process = process;
+        this.arrivalTime = process.getArrivalTime();
+        this.serviceTime = process.getServiceTime();
+        this.finishTime = null;
+        this.tat = null;
+        this.ntat = null;
+    }
+
+    public void setFinishTime(Integer finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public void setTat(Integer tat) {
+        this.tat = tat;
+    }
+
+    public void setNtat(Integer ntat) {
+        this.ntat = ntat;
+    }
+
+    public Integer getFinishTime() {
+        return finishTime;
+    }
+
+    public Integer getTat() {
+        return tat;
+    }
+
+    public Integer getNtat() {
+        return ntat;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getServiceTime() {
+        return serviceTime;
+    }
+
+    public Process getProcess() {
+        return process;
+    }
 }
